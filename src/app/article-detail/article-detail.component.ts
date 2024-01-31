859bd2629026d587dd373ee9ae4f78970d6e883a
mehmet <sayinmehmet47@gmail.com>
2024-01-31 17:52:20 +0100

    feat: search and loadmore functionality

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SpaceflightService } from '../spaceflight.service';
 import { ActivatedRoute } from '@angular/router';
 import { Article } from '../article';
+import { Subject } from 'rxjs';
+import { takeUntil, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-article-detail',
   templateUrl: './article-detail.component.html',
   styleUrls: ['./article-detail.component.css'],
 })
-export class ArticleDetailComponent {
+export class ArticleDetailComponent implements OnInit, OnDestroy {
   article: Article | undefined;
+  private unsubscribe$ = new Subject<void>();
 
   constructor(
     private spaceflightService: SpaceflightService,
@@ -17,13 +20,18 @@ export class ArticleDetailComponent {
   ) {}
 
   ngOnInit() {
-    this.getArticle();
+    this.route.paramMap
+      .pipe(
+        switchMap((params) =>
+          this.spaceflightService.getArticle(Number(params.get('id')))
+        ),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe((article) => (this.article = article as Article));
   }
 
-  getArticle(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.spaceflightService.getArticle(id).subscribe((article) => {
-      this.article = article as Article;
-    });
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,56 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SpaceflightService } from '../spaceflight.service';
 import { Articles } from '../article';
-import { Subject } from 'rxjs';
-import { debounceTime, switchMap } from 'rxjs/operators';
+import { Subject, BehaviorSubject } from 'rxjs';
+import { debounceTime, switchMap, tap, takeUntil } from 'rxjs/operators';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.css'],
 })
-export class ArticleListComponent {
+export class ArticleListComponent implements OnInit, OnDestroy {
   private static readonly INITIAL_ARTICLES_COUNT = 5;
   private static readonly ADDITIONAL_ARTICLES_COUNT = 2;
 
   limit = ArticleListComponent.INITIAL_ARTICLES_COUNT;
   offset = this.limit - ArticleListComponent.ADDITIONAL_ARTICLES_COUNT;
-  articles: Articles | undefined;
+  articles$ = new BehaviorSubject<Articles | null>(null);
   searchTerm: string | undefined;
   isLoading = false;
-  searchSubject = new Subject<string>();
+  searchForm = this.fb.group({
+    search: [''],
+  });
+  private destroy$ = new Subject<void>();
 
-  constructor(private spaceflightService: SpaceflightService) {
+  constructor(
+    private spaceflightService: SpaceflightService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit() {
     this.fetchArticles(this.limit);
 
-    this.searchSubject
-      .pipe(
+    this.searchForm
+      .get('search')!
+      .valueChanges.pipe(
         debounceTime(300),
         switchMap((searchTerm) => {
+          this.searchTerm = searchTerm || undefined;
           this.isLoading = true;
-          return this.spaceflightService.getArticles(this.limit, 0, searchTerm);
-        })
+          return this.spaceflightService.getArticles(
+            this.limit,
+            0,
+            searchTerm || undefined
+          );
+        }),
+        tap(() => (this.isLoading = false)),
+        takeUntil(this.destroy$)
       )
-      .subscribe({
-        next: (data) => {
-          this.articles = data as Articles;
-          this.isLoading = false;
-        },
-        error: (error) => {
-          console.error(error);
-          this.isLoading = false;
-        },
-      });
+      .subscribe((articles) => this.articles$.next(articles as Articles));
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onLoadMore() {
+    this.offset += ArticleListComponent.ADDITIONAL_ARTICLES_COUNT;
+    this.fetchArticles(
+      ArticleListComponent.ADDITIONAL_ARTICLES_COUNT,
+      this.offset,
+      this.searchTerm || undefined
+    );
   }
 
   fetchArticles(limit: number, offset?: number, searchTerm?: string) {
     this.isLoading = true;
     this.spaceflightService.getArticles(limit, offset, searchTerm).subscribe({
       next: (data) => {
-        if (this.articles && offset) {
-          this.articles.results.push(...(data as Articles).results);
+        let currentArticles = this.articles$.value;
+        if (currentArticles && offset) {
+          currentArticles.results.push(...(data as Articles).results);
+          this.articles$.next(currentArticles);
         } else {
-          this.articles = data as Articles;
+          this.articles$.next(data as Articles);
         }
         this.isLoading = false;
       },
@@ -59,19 +83,5 @@ export class ArticleListComponent {
         this.isLoading = false;
       },
     });
-  }
-
-  onLoadMore() {
-    this.offset += ArticleListComponent.ADDITIONAL_ARTICLES_COUNT;
-    this.fetchArticles(
-      ArticleListComponent.ADDITIONAL_ARTICLES_COUNT,
-      this.offset,
-      this.searchTerm
-    );
-  }
-
-  onSearch(searchTerm: string) {
-    this.searchTerm = searchTerm;
-    this.searchSubject.next(searchTerm);
   }
 }
